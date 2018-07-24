dd31b463a9ae7958b998f18203484ae1188e33be
aofadero <aofadero@gmail.com>
2018-07-23 23:03:06 -0400

    First commit for das-boot app

---
+++ src/main/java/com/jadant/App.java
@@ -0,0 +1,15 @@
+package com.jadant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application runner
+ *
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
